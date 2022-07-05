a46c157a487e8ce1009de402a83fac25c9334675
hung222 <90371118+hung222@users.noreply.github.com>
2022-07-05 23:54:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,3 @@ app.listen(process.env.PORT || 3000, () => {
 app.get("/", function(req, res){
     res.render("trangchu");
 });
-const mongoAtlasUri = "mongodb+srv://anhtbok:87654321@cluster0.i6xzm.mongodb.net/my_database?retryWrites=true&w=majority";
-
-try {
-    // Connect to the MongoDB cluster
-    mongoose.connect(
-        mongoAtlasUri,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        () => console.log(" Mongoose blog mindx is connected")
-    );
-} catch (e) {
-    console.log("could not connect");
-}
